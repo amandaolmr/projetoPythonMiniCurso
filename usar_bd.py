8e9ca0554ce897d95ebef943e3d1d12640284323
amandaolmr <amandinha.g.2009@hotmail.com>
2024-11-02 11:15:09 -0300

    Mini projeto python com banco de dados

---
+++ usar_bd.py
@@ -0,0 +1,15 @@
+import DAO
+
+#saida = DAO.inserir_usuario('user teste', 'teste@teste.com', 'senha123')
+#saida = DAO.inserir_usuario('Amanda', 'amanda@email.com', 'senhaAmanda')
+#saida = DAO.inserir_usuario('Joao', 'joao@email.com', 'senhaJoao')
+
+#saida = DAO.buscar_usuario('user teste')
+#saida = DAO.listar_usuarios()
+
+#saida = DAO.atualizar_usuario(2, 'Joao alterar', 'joaoAlterar@email.com')
+
+#saida = DAO.deletar_usuario(2)
+
+#saida = DAO.listar_usuarios()
+#print(saida)
